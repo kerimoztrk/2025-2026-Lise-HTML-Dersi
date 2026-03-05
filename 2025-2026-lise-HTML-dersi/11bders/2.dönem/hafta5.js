99e915453379a534a2b5415cfb883d33a4c477c0
Kerim Öztürk <kerimmozturk@hotmail.com>
2026-03-05 13:02:39 +0300

    güncelleme

--- 2025-2026-lise-HTML-dersi/11bders/2.dönem/hafta5.js
+++ 2025-2026-lise-HTML-dersi/11bders/2.dönem/hafta5.js
@@ -1,46 +1,104 @@
 //ZAMANLAYICILAR (SETİNTERVAL,SETTİMEOUT)
 
-function degistir(){
+// function degistir(){
 
-    setTimeout(function(){
-    document.getElementById("metin").innerText= "2 saniye sonra değişti"        
-    }, 10000);
-
-
-}
-
-function baslat(){
-
-    let sayi=0;
-
-    let timer=setInterval(function(){
-
-        sayi++;
-        document.getElementById("sayac").innerText=sayi;
-
-        if(sayi==10){
-            clearInterval(timer);
-        }
-    },1000);
-}
+//     setTimeout(function(){
+//     document.getElementById("metin").innerText= "2 saniye sonra değişti"        
+//     }, 10000);
 
 
+// }
+
+// function baslat(){
+
+//     let sayi=0;
+
+//     let timer=setInterval(function(){
+
+//         sayi++;
+//         document.getElementById("sayac").innerText=sayi;
+
+//         if(sayi==10){
+//             clearInterval(timer);
+//         }
+//     },1000);
+// }
 
 
-$("#btn").click(function(){
-    $("#yazi").text("JQuery ile değişti");
-});
 
 
-$("#renkbtn").click(function(){
-    $("#metin2").css("color","red");
-});
+// $("#btn").click(function(){
+//     $("#yazi").text("JQuery ile değişti");
+// });
 
 
-$("#gizle").click(function(){
-    $("#kutu").hide();
-});
+// $("#renkbtn").click(function(){
+//     $("#metin2").css("color","red");
+// });
 
-$("#goster").click(function(){
-    $("#kutu").show();
-});
+
+// $("#gizle").click(function(){
+//     $("#kutu").hide();
+// });
+
+// $("#goster").click(function(){
+//     $("#kutu").show();
+// });
+
+
+
+// let sayi = 10;
+// let timer;
+
+// $("#baslat").click(function(){
+
+//     timer = setInterval(function(){
+
+//         $("#ekran").text(sayi);
+//         sayi--;
+
+//         if(sayi < 0){
+//             clearInterval(timer);
+
+//             setTimeout(function(){
+//                 alert("Süre Bitti!");
+//             },500);
+
+//         }
+
+//     },1000);
+
+// });
+
+
+
+
+
+// function aBaslat(){
+//  let renkler=["red","blue","green","orange","purple"];
+//  setInterval(function(){
+//     let rastgeleSayi=Math.floor(Math.random()*renkler.length);
+//     document.body.style.backgroundColor=renkler[rastgeleSayi];
+//  },1000);
+// }
+
+
+
+// $("#buyut").click(function(){
+//     $("#metin3").css("font-size","50px");
+// });
+
+
+// $("#degistir").click(function(){
+//     $("#resim").attr("src","fotograflar/resim2.jpg");
+
+// });
+
+
+
+setInterval(function(){
+    let zaman= new Date();
+
+    document.getElementById("saat").innerHTML=zaman.toLocaleTimeString();
+
+},1000);
